55a564b86be0f6e365f1fa5440e334d4ac92ddbf
Nikoloz <nikoloz.palagashvili@gmail.com>
2023-07-05 20:02:34 +0400

    started repeating from states props and reerenders

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,32 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Cocktails } from "./pages/Cocktails";
 
 import "./App.css";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 
 import { SliderPage } from "./pages/Slider";
+import { HomePage } from "./pages/HomePage";
+import { useState } from "react";
 
 function App() {
+	const [number, setNumber] = useState(0);
 	return (
 		<>
-			<BrowserRouter>
+			<HomePage count={number} changeNumber={setNumber} />
+			<Cocktails />
+			{/* <BrowserRouter>
 				<header>
-					<Link to="/about">Aboyt</Link>
+					<Link to="/about">About</Link>
 				</header>
 				<Routes>
-					<Route path="/" element={<div>Home</div>} />
+					<Route
+						path="/"
+						element={<HomePage count={number} changeNumber={setNumber} />}
+					/>
 					<Route path="/cocktails" element={<Cocktails />} />
 					<Route path="/counter" element={<div>Counter: 0</div>} />
 					<Route path="/swiper" element={<SliderPage />} />
 					<Route path="*" element={<div>Not Found</div>} />
 				</Routes>
-			</BrowserRouter>
+			</BrowserRouter> */}
 		</>
 	);
 }
--- src/pages/Slider.tsx
+++ src/pages/Slider.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -10,13 +11,14 @@ import "swiper/css/navigation";
 // import "./styles.css";
 
 // import required modules
+// @ts-ignore
 import { Pagination, Navigation } from "swiper";
 
 export const SliderPage = () => {
 	const pagination = {
 		clickable: true,
-		renderBullet: function (index, className) {
-			return '<span class="' + " gela" + '">' + (index + 1) + "</span>";
+		renderBullet: function (index: number, className: string) {
+			return '<span class="' + className + '">' + (index + 1) + "</span>";
 		},
 	};
 
@@ -29,37 +31,28 @@ export const SliderPage = () => {
 				className="mySwiper"
 				loop
 			>
-				<SwiperSlide></SwiperSlide>
 				<SwiperSlide>
-					{" "}
 					<div style={{ height: 400, backgroundColor: "red" }}>Slide 1</div>
 				</SwiperSlide>
 				<SwiperSlide>
-					{" "}
 					<div style={{ height: 400, backgroundColor: "red" }}>Slide 2</div>
 				</SwiperSlide>
 				<SwiperSlide>
-					{" "}
 					<div style={{ height: 400, backgroundColor: "red" }}>Slide 3</div>
 				</SwiperSlide>
 				<SwiperSlide>
-					{" "}
 					<div style={{ height: 400, backgroundColor: "red" }}>Slide 4</div>
 				</SwiperSlide>
 				<SwiperSlide>
-					{" "}
 					<div style={{ height: 400, backgroundColor: "red" }}>Slide 5</div>
 				</SwiperSlide>
 				<SwiperSlide>
-					{" "}
 					<div style={{ height: 400, backgroundColor: "red" }}>Slide 6</div>
 				</SwiperSlide>
 				<SwiperSlide>
-					{" "}
 					<div style={{ height: 400, backgroundColor: "red" }}>Slide 7</div>
 				</SwiperSlide>
 				<SwiperSlide>
-					{" "}
 					<div style={{ height: 400, backgroundColor: "red" }}>Slide 8</div>
 				</SwiperSlide>
 			</Swiper>
